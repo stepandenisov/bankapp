7b8ad71d90d88e533c9582f3b4d5a8e8281ed159
stepandenisov <stdnsv@gmail.com>
2025-11-13 14:10:48 +0400

    fix exchange controller

--- front/src/main/java/ru/yandex/front/controller/ExchangeController.java
+++ front/src/main/java/ru/yandex/front/controller/ExchangeController.java
@@ -1,15 +1,15 @@
 package ru.yandex.front.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.front.model.ExchangeRate;
 import ru.yandex.front.service.CurrencyService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/exchange")
 @RequiredArgsConstructor
 public class ExchangeController {
